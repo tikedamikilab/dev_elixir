4077240dce87dffcea2a400d7c100846d6ba2cbd
tikedamikilab <tikeda@mikilab.doshisha.ac.jp>
2020-07-10 08:05:06 +0900

    concat 関数ヘッド

--- Concat.ex
+++ Concat.ex
@@ -1,4 +1,10 @@
 defmodule Concat do
+    def join(a, b \\ nil, sep \\ " ")
+    
+    def join(a, b, _sep) when is_nil(b) do
+        a
+    end
+
     def join(a, b, sep \\ " ") do
         a <> sep <> b
     end
