b81903da622f45e9ee8d731061e0d5171f685b7e
tikedamikilab <tikeda@mikilab.doshisha.ac.jp>
2020-07-09 18:52:17 +0900

    math コメントアウト

--- math.ex
+++ math.ex
@@ -1,17 +1,18 @@
 defmodule Math do
-    def sum(a, b) do
-        do_sum(a, b)
-    end
 
-    def zero?(0) do
-        true
-    end
+    # def sum(a, b) do
+    #     do_sum(a, b)
+    # end
 
-    defp do_sum(a, b) do
-        a + b
-    end
+    # def zero?(0) do
+    #     true
+    # end
 
-    def zero?(x) when is_integer(x) do
-        false
-    end
+    # defp do_sum(a, b) do
+    #     a + b
+    # end
+
+    # def zero?(x) when is_integer(x) do
+    #     false
+    # end
 end
