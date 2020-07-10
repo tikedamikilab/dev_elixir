a47f07ca2a176839070704f671bd1855376e8bb5
tikedamikilab <tikeda@mikilab.doshisha.ac.jp>
2020-07-10 10:44:53 +0900

    mathlistsum コンパイル

---
+++ MathListSum.ex
@@ -0,0 +1,9 @@
+defmodule MathListSum do
+    def sum_list([head | tail], accumlator) do
+        sum_list(tail, head + accumlator)
+    end
+
+    def sum_list([], accumlator) do
+        accumlator
+    end
+end
